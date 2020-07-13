949c73c90173a15e3b5d158e12c46622b5ae4e42
Rosie Magura <magurarm@guilford.edu>
2020-07-13 14:11:31 -0400

    Notes on creating store

---
+++ index.js
@@ -0,0 +1,22 @@
+function createStore() {
+    let state
+    let listeners = []
+
+    const getState = () => state
+
+    const subscribe = (listener) => {
+        listeners.push(listener)
+    } 
+    return {
+        getState,
+        subscribe
+    }     
+}
+
+const store = createStore()
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
+store.subscribe(() => {
+    console.log('The store changed')
+})
